1c6593d288efca472f5b48ace6911757aa523f6a
afr2903 <afr102903@gmail.com>
2024-04-12 12:38:01 -0600

    refactor: Changed pick action server handler

--- ws/src/frida_task_manager/scripts/manipulation_tasks.py
+++ ws/src/frida_task_manager/scripts/manipulation_tasks.py
@@ -18,12 +18,20 @@ PLACE_TARGET = -5
 
 class TasksManipulation:
 
+    STATE = {
+        "TERMINAL_ERROR": -1,
+        "EXECUTION_ERROR": 0,
+        "EXECUTION_SUCCESS": 1
+    }
+
     AREA_TASKS = ["pick", "place", "grab", "give", "open", "close"]
 
     OBJECTS_DICT = {
         "zucaritas": 1,
-        "cookies": 9,
-        "cookie": 9
+        "cookies": 11,
+        "cookie": 11,
+        "snack": 5,
+        "snacks": 5
     }
 
     def __init__(self) -> None:
@@ -35,40 +43,37 @@ class TasksManipulation:
         """Method to execute each command"""
         rospy.loginfo("Manipulation Command")
         if command == "pick":
-            return self.execute_pick_and_place( TasksManipulation.OBJECTS_DICT[target] )
+            return self.execute_pick( target )
         if command == "place":
             return self.execute_pick_and_place(PLACE_TARGET)
 
         return -1
     
+    def execute_pick(self, target: str) -> int:
+        """Method to execute the pick action"""
+        if target not in TasksManipulation.OBJECTS_DICT:
+            rospy.logerr("Object not found")
+            return TasksManipulation.STATE["EXECUTION_ERROR"]
+        return self.execute_pick_and_place( TasksManipulation.OBJECTS_DICT[target] )
+
     def execute_pick_and_place(self, target: int) -> int:
         """Method to call the pick and place action server"""
-        class ManipulationGoalScope:
-            object_ = target
-            result = False
-            result_received = False
-        
+
         def manipulation_goal_feedback(feedback_msg):
             pass
-        
-        def get_result_callback(state, result):
-            ManipulationGoalScope.result = result.result
-            ManipulationGoalScope.result_received = True
 
         rospy.loginfo("Sending Manipulation Goal")
 
         self.pick_client.send_goal(
-                    manipulationPickAndPlaceGoal(object_id = ManipulationGoalScope.object_),
+                    manipulationPickAndPlaceGoal(object_id = target),
                     feedback_cb=manipulation_goal_feedback,
-                    done_cb=get_result_callback)
-        
+            )
+
         rospy.loginfo(f"Object ID: {target}")
-        
-        while not ManipulationGoalScope.result_received and not rospy.is_shutdown():
-            pass
-        
-        return 1 if ManipulationGoalScope.result else -1
-        
+        self.pick_client.wait_for_result()
+        result = self.pick_client.get_result()
+        return TasksManipulation.STATE["EXECUTION_SUCCESS"] if result.result else TasksManipulation.STATE["EXECUTION_ERROR"]
+
     def cancel_command(self) -> None:
         """Method to cancel the current command"""
         self.pick_client.cancel_all_goals()
@@ -78,5 +83,4 @@ if __name__ == "__main__":
     try:
         TasksManipulation()
     except rospy.ROSInterruptException as e:
-        rospy.logerr("Error: {}".format(e))
-        pass
+        rospy.logerr(f'Error: {e}')
