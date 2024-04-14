b285d8d422e8388c8cb2bdc67f2b599348b9a3a4
afr2903 <afr102903@gmail.com>
2024-04-14 05:22:17 -0600

    feat: Face following with arm working

--- ws/src/frida_task_manager/scripts/receptionist_task_manager.py
+++ ws/src/frida_task_manager/scripts/receptionist_task_manager.py
@@ -12,20 +12,22 @@ import actionlib
 from std_msgs.msg import String
 from frida_hri_interfaces.msg import Command, CommandList
 from frida_hri_interfaces.msg import ConversateAction, ConversateFeedback, ConversateGoal, ConversateResult
+from frida_vision_interfaces.msg import Person, PersonList
 
 ### Python submodules
 from hri_tasks import TasksHRI
 from manipulation_tasks import TasksManipulation
 from nav_tasks import TasksNav
+from vision_tasks import TasksVision
 
 SPEAK_TOPIC = "/speech/speak"
 CONVERSATION_SERVER = "/conversation_as"
-FACE_LOCATIONS_TOPIC = "/
+FACE_LOCATIONS_TOPIC = "/person_list"
 
-NAV_ENABLED = True
+NAV_ENABLED = False
 MANIPULATION_ENABLED = True
 CONVERSATION_ENABLED = False
-VISION_ENABLED = False
+VISION_ENABLED = True
 
 AREAS = ["nav", "manipulation", "hri", "vision"]
 
@@ -36,17 +38,17 @@ AREA_ENABLED = {
     "vision": VISION_ENABLED
 }
 
-class TaskManagerServer:
-    """Class to manage different tasks divided by categories"""
-    STATE_ENUM = {
-        "IDLE": 0,
-        "RECEIVE_COMMANDS": 1,
-        "EXECUTING_COMMANDS": 2,
-        "STOPPING": 3,
-        "ERROR": 4,
-        "SHUTDOWN": 5
-    }
+STATES = {
+    "WAITING_GUEST": 0,
+    "SELF_INTRODUCTION": 1,
+    "REQUEST_GUEST_INFORMATION": 2,
+    "STORE_INFORMATION": 3,
+    "ERROR": 4,
+    "SHUTDOWN": 5
+}
 
+class ReceptionistTaskManager:
+    """Class to manage different tasks divided by categories"""
     COMMANDS_CATEGORY = {
         "nav" : ["go", "follow", "stop", "approach", "remember"],
         "manipulation" : ["pick", "place", "grasp", "give", "open", "close", "pour"],
@@ -57,7 +59,9 @@ class TaskManagerServer:
     def __init__(self) -> None:
         self._node = rospy.init_node("task_manager_server")
         self._rate = rospy.Rate(200)
-        self._sub = rospy.Subscriber(COMMANDS_TOPIC, CommandList, self.commands_callback)
+        
+        self.last_time = rospy.Time.now()
+        self.follow_sample_time = 0.5
 
         # Creates an empty dictionary to store the subtask manager of each area
         self.subtask_manager = dict.fromkeys(AREAS, None)
@@ -69,76 +73,42 @@ class TaskManagerServer:
             self.subtask_manager["manipulation"] = TasksManipulation()
         if NAV_ENABLED:
             self.subtask_manager["nav"] = TasksNav()
-        #if VISION_ENABLED:
-            #self.subtask_manager["vision"] = TasksVision()
+        if VISION_ENABLED:
+            self.subtask_manager["vision"] = TasksVision()
+            rospy.Subscriber(FACE_LOCATIONS_TOPIC, PersonList, self.get_face_locations)
 
-        self.current_state = TaskManagerServer.STATE_ENUM["IDLE"]
+        self.current_state = STATES["WAITING_GUEST"]
         self.current_past_state = None
         self.current_command = None
         self.current_queue = []
         self.perceived_information = ""
 
-        self.current_queue = [
-            Command(action="wait", complement="person"),
-            Command(action="analyze", complement="person"),
-            Command(action="interact", complement="introduce yourself"),
-            Command(action="ask", complement="user name and favorite drink"),
-            Command(action="save", complement="name"),
-            Command(action="interact", complement="ask to be followed to the living room"),
-            Command(action="go", complement="living room"),
-            Command(action="look", complement="seat"),
-            Command(action="interact", complement="introduce host"),
-            Command(action="look", complement="guest 1"),
-            Command(action="interact", complement="announce a place to sit will be offered"),
-            Command(action="look", complement="seat"),
-            Command(action="find", complement="free seat"),
-            Command(action="look", complement="free seat"),
-            Command(action="interact", complement="indicate the user can sit in the direction is gazing"),
-            Command(action="interact", complement="announce robot will return to entrance"),
-            Command(action="go", complement="entrance"),
+        self.detected_faces = []
+        self.following_face = True
+        self.followed_person = "Unknown"
+        self.arm_moving = False
 
-            Command(action="wait", complement="person"),
-            Command(action="analyze", complement="person"),
-            Command(action="interact", complement="introduce yourself"),
-            Command(action="ask", complement="user name and favorite drink"),
-            Command(action="save", complement="name"),
-            Command(action="interact", complement="ask to be followed to the living room"),
-            Command(action="go", complement="living room"),
-            Command(action="look", complement="seat"),
-            Command(action="interact", complement="introduce host"),
-            Command(action="interact", complement="introduce guest 1"),
-            Command(action="look", complement="guest 2"),
-            Command(action="interact", complement="announce a place to sit will be offered"),
-            Command(action="look", complement="seat"),
-            Command(action="find", complement="free seat"),
-            Command(action="look", complement="free seat"),
-            Command(action="interact", complement="indicate the user can sit in the direction is gazing")
-        ]
+        self.current_queue = []
 
         self.run()
 
-    def commands_callback(self, commands_input: CommandList) -> None:
-        """Receive processed commands from the interpreter and call executions from the queue"""
-        rospy.loginfo("Received commands")
+    def get_face_locations(self, data: PersonList) -> None:
+        """Callback to receive the face locations"""
+        self.detected_faces = data.list
 
-        if not commands_input:
-            self.current_state = TaskManagerServer.STATE_ENUM["ERROR"]
-            return
-
-        if self.current_thread is not None:
-            return
-
-        if self.current_state != TaskManagerServer.STATE_ENUM["IDLE"]:
-            rospy.logerr("Cancelling current commands and executing new received ")
-            self.current_queue = []
-            self.cancel_command()
-            self.current_state = TaskManagerServer.STATE_ENUM["IDLE"]
-
-        self.current_state = TaskManagerServer.STATE_ENUM["RECEIVE_COMMANDS"]
-        self.current_queue = commands_input.commands
-        self.past_state = self.current_state
-
-        #self.say("I 'have finished my tasks, I'm going to rest now")
+    def follow_face(self) -> bool:
+        """Calls the arm joints server to follow a face
+        Returns: Movement of the arm executed"""
+        if self.following_face and not self.arm_moving and self.detected_faces:
+            rospy.loginfo(f"Following {self.followed_person}")
+            self.arm_moving = True
+            for face in self.detected_faces:
+                if face.name == self.followed_person:
+                    self.subtask_manager["manipulation"].move_arm_joints(face.x, face.y)
+                    self.arm_moving = False
+                    self.detected_faces = []
+                    return True
+        return False
 
     def execute_command(self, command: Command) -> int:
         """Method for executing a single command inside its area submodule"""
@@ -147,50 +117,40 @@ class TaskManagerServer:
 
         task_result = 0
         for area in AREAS:
-            if command.action in TaskManagerServer.COMMANDS_CATEGORY[area] and AREA_ENABLED[area]:
+            if command.action in ReceptionistTaskManager.COMMANDS_CATEGORY[area] and AREA_ENABLED[area]:
                 task_result = self.subtask_manager[area].execute_command(
                     command.action, command.complement, self.perceived_information
                 )
 
         if task_result == -1:
             rospy.logerr("Error in task execution")
-            return TaskManagerServer.STATE_ENUM["ERROR"]
+            return STATES["ERROR"]
 
         self.perceived_information += f"{command.action} {command.complement} {task_result} "
-        return TaskManagerServer.STATE_ENUM["EXECUTING_COMMANDS"]
+        return STATES["EXECUTING_COMMANDS"]
 
     def cancel_command(self) -> None:
         """Method to cancel the current command"""
         for area in AREAS:
-            if self.current_command in TaskManagerServer.COMMANDS_CATEGORY[area]:
+            if self.current_command in ReceptionistTaskManager.COMMANDS_CATEGORY[area]:
                 self.subtask_manager[area].cancel_command()
 
     def run(self) -> None:
         """Main loop for the task manager"""
         while not rospy.is_shutdown():
+            dt = rospy.Time.now() - self.last_time # Time from last iteration
 
-            if len(self.current_queue) > 0: #and self.current_thread == current_thread:
-                self.current_command = self.current_queue.pop(0)
-                self.status = self.execute_command(self.current_command)
-                if self.status == TaskManagerServer.STATE_ENUM["ERROR"]:
-                    rospy.logerr("Error in command manager")
-                    self.current_state = TaskManagerServer.STATE_ENUM["ERROR"]
-                    self.cancel_command()
-                    self.current_queue = []
-                    self.state = TaskManagerServer.STATE_ENUM["IDLE"]
-                    self.past_state = self.status
-                    continue
-            elif self.current_state != TaskManagerServer.STATE_ENUM["IDLE"]:
-                self.current_thread = None
-                self.current_queue = []
-                self.current_state = TaskManagerServer.STATE_ENUM["IDLE"]
-                
-                #self.subtask_manager["hri"].speak("I have finished my tasks, I'm going to rest now")
+            if self.current_state == STATES["WAITING_GUEST"]:
+                if dt.to_sec() > self.follow_sample_time:
+                    self.last_time = rospy.Time.now()
+                    if self.follow_face():
+                        self.current_state = STATES["SELF_INTRODUCTION"]
+
             self._rate.sleep()
 
 
 if __name__ == "__main__":
     try:
-        TaskManagerServer()
+        ReceptionistTaskManager()
     except rospy.ROSInterruptException as e:
         rospy.logerr(f'Error: {e}')
